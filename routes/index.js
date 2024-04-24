464d6c15eed105f5149dbc62ed2077f74e7495ae
robmar00246 <robmar00246@stud.noroff.no>
2024-04-24 22:23:26 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const { Router } = require("express");
+const { save } = require("../save_json");
+let favouriteNumber = require("../number.json");
+const add = require("../add");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = new Router();
+
+router.get("/sum/:number1/:number2", async (req, res) => {
+  let my_file = await s3.getObject({
+    Bucket: "cyclic-puzzled-mite-attire-eu-north-1",
+    Key: "number.json",
+  }).promise()
+  const favNumber = JSON.parse(my_file.Body)?.favouriteNumber;
+  const {number1, number2} = req.params;
+  if(number1 == null || number2 == null) {
+    res.status(400).send("Not provided numbers");
+    return;
+  }
+  if(isNaN(parseInt(number1)) || isNaN(parseInt(number2))) {
+    res.status(400).send("Numbers needs to be integer");
+    return;
+  }
+  let result = add(parseInt(number1), parseInt(number2));
+  if(favNumber != null) {
+    result = add(result, favNumber )
+  }
+  res.json({
+    status: "success",
+    result: result,
+  });
+});
+
+router.post("/favNumber", async (req, res) => {
+  const {number} = req.body;
+  if(number == null ) {
+    res.status(400).send("Not provided number");
+    return;
+  }
+  if(isNaN(parseInt(number))) {
+    res.status(400).send("The number needs to be integer");
+    return;
+  }
+  await save({
+    favouriteNumber: number
+  });
+  res.json({
+    status: "success",
+    newFavouriteNumber: number,
+  });
 });
 
 module.exports = router;
